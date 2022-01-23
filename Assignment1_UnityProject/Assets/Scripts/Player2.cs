9dc508df413ea00b74142095f988c77438b138c7
Suikern <68206572+Suikern@users.noreply.github.com>
2022-01-23 18:43:25 +0800

    added unity chan
    added unity chan and added her ground movement ad jump(doesnt work properly rn)

---
+++ Assignment1_UnityProject/Assets/Scripts/Player2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PGGE.Patterns;
+using PGGE;
+
+public class Player2 : MonoBehaviour
+{
+    [HideInInspector]
+    public FSM mFsm = new FSM();
+    public Animator mAnimator;
+    public PlayerMovement mPlayerMovement;
+
+    public LayerMask mPlayerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mFsm.Add(new PlayerState_MOVEMENT(this));
+        mFsm.SetCurrentState((int)PlayerStateType.MOVEMENT);
+
+        PlayerConstants.PlayerMask = mPlayerMask;
+    }
+
+    void Update()
+    {
+        mFsm.Update();
+    }
+
+    public void Move()
+    {
+        mPlayerMovement.HandleInputs();
+        mPlayerMovement.Move();
+    }
+}
