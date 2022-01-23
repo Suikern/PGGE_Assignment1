d50ad79d822e4127bd963ce7c1d3932237e1e693
Suikern <68206572+Suikern@users.noreply.github.com>
2022-01-23 22:11:42 +0800

    started on step
    currently only on concrete and walking sounds

---
+++ Assignment1_UnityProject/Assets/Scripts/Footstep.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footstep : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] mAudioClips;
+
+    private AudioSource mAudioSource;
+    private void Awake()
+    {
+        mAudioSource = GetComponent<AudioSource>();
+    }
+    private void Step() //Funtion is called during an event during the animation of walking
+    {
+        AudioClip clip = GetRandomClip();
+        mAudioSource.PlayOneShot(clip);
+    }
+    
+
+    private AudioClip GetRandomClip()
+    {
+        return mAudioClips[Random.Range(0, mAudioClips.Length)];
+    }
+}
