78933da340868aa7d098aea272b62a42a9e01743
Suikern <68206572+Suikern@users.noreply.github.com>
2022-01-24 02:44:33 +0800

    changing footsteps
    sphagetti code but works

--- Assignment1_UnityProject/Assets/Scripts/Footstep.cs
+++ Assignment1_UnityProject/Assets/Scripts/Footstep.cs
@@ -6,23 +6,106 @@ using UnityEngine;
 public class Footstep : MonoBehaviour
 {
     [SerializeField]
-    private AudioClip[] mAudioClips;
+    private AudioClip[] concreteAudioClips;
+    [SerializeField]
+    private AudioClip[] dirtAudioClips;
+    [SerializeField]
+    private AudioClip[] metalAudioClips;
+    [SerializeField]
+    private AudioClip[] sandAudioClips;
+    [SerializeField]
+    private AudioClip[] woodAudioClips;
 
     private AudioSource mAudioSource;
+
     private void Awake()
     {
         mAudioSource = GetComponent<AudioSource>();
     }
     private void Step() //Funtion is called during an event during the animation of walking
     {
-        AudioClip clip = GetRandomClip();
-        mAudioSource.PlayOneShot(clip);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 3))
+        {
+
+            if (hit.transform.CompareTag("Concrete"))
+            {
+                int index = Random.Range(0, concreteAudioClips.Length);
+                AudioClip clip = concreteAudioClips[index];
+                mAudioSource.PlayOneShot(clip);
+            }
+            if (hit.transform.CompareTag("Dirt"))
+            {
+                int index = Random.Range(0, dirtAudioClips.Length);
+                AudioClip clip = dirtAudioClips[index];
+                mAudioSource.PlayOneShot(clip);
+            }
+            if (hit.transform.CompareTag("Metal"))
+            {
+                int index = Random.Range(0, metalAudioClips.Length);
+                AudioClip clip = metalAudioClips[index];
+                mAudioSource.PlayOneShot(clip);
+            }
+            if (hit.transform.CompareTag("Sand"))
+            {
+                int index = Random.Range(0, sandAudioClips.Length);
+                AudioClip clip = sandAudioClips[index];
+                mAudioSource.PlayOneShot(clip);
+            }
+            if (hit.transform.CompareTag("Wood"))
+            {
+                int index = Random.Range(0, woodAudioClips.Length);
+                AudioClip clip = woodAudioClips[index];
+                mAudioSource.PlayOneShot(clip);
+            }
+        }
+        //CheckTerrain();
+        //AudioClip clip = GetRandomClip(dirtAudioClips);
+        //mAudioSource.PlayOneShot(clip);
     }
     
 
-    private AudioClip GetRandomClip()
-    {
-        int index = Random.Range(0, mAudioClips.Length);
-        return mAudioClips[index];
-    }
+    //private AudioClip GetRandomClip(AudioClip[] a)
+    //{
+    //    int index = Random.Range(0, a.Length);
+    //    return a[index];
+    //}
+    //private AudioClip CheckTerrain()
+    //{
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(transform.position, Vector3.down, out hit, 3))
+    //    {
+
+    //        if (hit.transform.CompareTag("Concrete"))
+    //        {
+    //            int index = Random.Range(0, concreteAudioClips.Length);
+    //            return concreteAudioClips[index];
+    //        }
+    //        if (hit.transform.CompareTag("Dirt"))
+    //        {
+    //            int index = Random.Range(0, dirtAudioClips.Length);
+    //            return dirtAudioClips[index];
+    //        }
+    //        if (hit.transform.CompareTag("Metal"))
+    //        {
+    //            int index = Random.Range(0, metalAudioClips.Length);
+    //            return metalAudioClips[index];
+    //        }
+    //        if (hit.transform.CompareTag("Sand"))
+    //        {
+    //            int index = Random.Range(0, sandAudioClips.Length);
+    //            return sandAudioClips[index];
+    //        }
+    //        if (hit.transform.CompareTag("Wood"))
+    //        {
+    //            int index = Random.Range(0, woodAudioClips.Length);
+    //            return woodAudioClips[index];
+    //        }
+    //    }
+    //    else
+    //    {
+    //        int index = Random.Range(0, woodAudioClips.Length);
+    //        return woodAudioClips[index];
+    //    }
+    //}
 }
